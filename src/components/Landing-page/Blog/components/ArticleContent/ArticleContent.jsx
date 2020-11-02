2e8b60774eb8b01358b2f76141bfa5080adcb364
Olamide Sholanke <olamidesholanke@MacBook-Pro.local>
2020-11-02 10:56:22 +0100

    feat : added authors data

--- src/components/Landing-page/Blog/components/ArticleContent/ArticleContent.jsx
+++ src/components/Landing-page/Blog/components/ArticleContent/ArticleContent.jsx
@@ -10,11 +10,7 @@ export default function ArticleContent() {
     posts: { posts },
   } = usePostsContext();
 
-
-
   const [postContent, setPostContent] = useState();
-
-
 
   useEffect(() => {
     if (posts.length > 0)
@@ -23,15 +19,12 @@ export default function ArticleContent() {
       );
   }, [uuidParam, posts]);
 
-
-
   useEffect(() => {
     if (posts.length > 0) {
       const post = [...posts].filter((post) => post.fields.slug === uuid)[0];
       setPostContent(post?.fields);
     }
   }, [uuid, posts]);
-
 
   return (
     !!postContent && (
@@ -48,14 +41,27 @@ export default function ArticleContent() {
             {moment(postContent?.publishDate, "YYYYMMDD").fromNow()}
           </p>
         </div>
-        <p className="article_title">
-          {postContent?.title} : {postContent?.description}
-        </p>
-        <p className="article_writer">
-          <span className="dp_rapper"></span>
-          Sholanke Olamide
-        </p>
-        <br/>
+        <p className="article_title">{postContent?.title}</p>
+        <p className="article_desc">{postContent?.description}</p>
+        <div className="article_write_container">
+          <p className="title">About the author</p>
+          <div className="rapper">
+            <p className="article_writer">
+              <span
+                className="dp_rapper"
+                style={{
+                  backgroundImage: `url("https:${postContent?.author.fields.image.fields.file.url}")`,
+                }}
+              ></span>
+              {postContent?.author.fields.name}
+            </p>
+
+            <p className="article_writer_desc">
+              "{postContent?.author.fields.shortBio}"
+            </p>
+          </div>
+        </div>
+
         <p>{postContent.body}</p>
       </div>
     )
