b89cdbeddb9c26340fc81d586bb96aad3f7b3400
Olamide Sholanke <olamidesholanke@MacBook-Pro.local>
2021-01-05 02:58:42 +0100

    scroll to top on page change

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,16 +1,26 @@
-import React from "react";
-import { HashRouter, Route, Redirect } from "react-router-dom";
+import React, { useEffect } from "react";
+import { HashRouter, Route, Redirect, useLocation } from "react-router-dom";
 import LandingPage from "./Landing-page";
 import AllArticles from "./AllArticles/AllArticles";
 
 export default function Routes() {
   return (
     <HashRouter>
-      <Route path="/" exact>
-        <Redirect to="/articles" />
-      </Route>
-      <Route path="/articles" exact component={AllArticles} />
-      <Route path="/articles/:uuid" exact component={LandingPage} />
+      <RoutesWrapper>
+        <Route path="/" exact>
+          <Redirect to="/articles" />
+        </Route>
+        <Route path="/articles" exact component={AllArticles} />
+        <Route path="/articles/:uuid" exact component={LandingPage} />
+      </RoutesWrapper>
     </HashRouter>
   );
+}
+
+function RoutesWrapper({ children }) {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  return children;
 }
